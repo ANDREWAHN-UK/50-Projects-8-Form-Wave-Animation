a4442a171b13974145ee902f9b30d8dcac1c83c0
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2022-07-13 09:07:44 +0000

    script.js - add code to make sure each letter has the effect applied sequentially, so it looks like a mexican wave

--- assets/script.js
+++ assets/script.js
@@ -4,7 +4,8 @@ labels.forEach(
     label=> {
         label.innerHTML = label.innerText                //loops through the labels,
         .split('')                                       //splits them, i.e. separates each letter
-        .map((letter, idx) =>`<span>${letter}</span>`)   //then wraps each letters with a span
+        .map((letter, idx) =>`<span style="transition-delay:${idx * 100}ms">${letter}</span>`)   
+        //then wraps each letters with a span, and goes through the span to apply the transition
         .join('')                                        //joins each letter, with a span around it, into a string
     }
 )
